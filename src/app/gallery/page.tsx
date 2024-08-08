210def109cdbce34338ecde97d7a930c937825bf
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-08 09:31:14 +0100

    fix error gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import GalleryPage from '../components/organisms/GalleryPage';
 import { artworksData } from '../../data/artworks'; 
 
+
+const sanitizedArtworks = artworksData.map((artwork) => ({
+  ...artwork,
+  src: artwork.src || '/my-ecommerce-app/public/search.jpg', 
+}));
+
 const Gallery: React.FC = () => {
   return (
-    <GalleryPage artworks={artworksData} />
+    <GalleryPage artworks={sanitizedArtworks} />
   );
 };
 
