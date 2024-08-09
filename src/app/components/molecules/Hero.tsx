ed77daafc02381d04aa61e6678e53f3af3debaee
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-09 16:00:08 +0100

    add mobile bg hero

--- src/app/components/molecules/Hero.tsx
+++ src/app/components/molecules/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="relative bg-hero min-h-screen flex items-center justify-center">
+    <section className="relative bg-hero min-h-[74vh] md:min-h-screen flex items-center justify-center">
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div className="relative z-10 text-center space-y-4 px-4">
         <h1 className="text-white text-6xl md:text-8xl">Its all about ART.</h1>
