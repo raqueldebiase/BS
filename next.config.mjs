864d38dc908bcebd0c748403af70549c714f971a
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-12 11:20:25 +0100

    add formats webp and avif

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    
+    formats: ['image/webp', 'image/avif'], 
+  },
 };
 
 export default nextConfig;
