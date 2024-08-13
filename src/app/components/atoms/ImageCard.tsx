3d75a6f1594b2d3327cfd0e9e188f2a4be4769a4
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-13 10:43:14 +0100

    add new atrib to image talking abaout

--- src/app/components/atoms/ImageCard.tsx
+++ src/app/components/atoms/ImageCard.tsx
@@ -10,16 +10,20 @@ interface ImageCardProps {
 
 const ImageCard: React.FC<ImageCardProps> = ({ src, title, description }) => {
   return (
-    <div className="relative h-full group ">
+    <div className="relative h-full group overflow-hidden">
       <div className="relative h-full">
         <Image
           src={src}
-          alt={`Image - ${title}`}
-          className=""
+          alt={`Image of ${title}`}
+          layout="fill"
+          objectFit="cover"
+          placeholder="blur"
+          blurDataURL={src.src} // Assumindo que src.src é um placeholder desfocado
+          className="transition-transform duration-300 group-hover:scale-105"
         />
       </div>
-      <div className="absolute inset-0 bg-black bg-opacity-20 flex items-end justify-center">
-        <div className="bg-black bg-opacity-50 text-white p-2 w-full">
+      <div className="absolute inset-0 bg-black bg-opacity-30 flex items-end justify-center">
+        <div className="bg-black bg-opacity-60 text-white p-2 w-full">
           <h4 className="text-md font-light mb-2">{title}</h4>
           <p className='font-extralight tracking-wide text-sm truncate'>{description}</p>
         </div>
