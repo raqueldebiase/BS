98d5ab8ac1d6491998b508f1535eabf378ad5b70
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-13 11:28:23 +0100

    add loading lazy to images and change img search

--- src/app/components/organisms/Spotlight.tsx
+++ src/app/components/organisms/Spotlight.tsx
@@ -1,57 +1,46 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import ButtonMore from '../atoms/ButtonMore';
 import Image1 from '../../../../public/spotlight/1.webp';
 import Image2 from '../../../../public/spotlight/tessla.webp';
-import Image3 from '../../../../public//gallery/retratoModerno.webp';
+import Image3 from '../../../../public/gallery/retratoModerno.webp';
 
 const Spotlight = () => {
-  // Convertendo as imagens importadas em URLs válidas
-  const imageUrl1 = Image1.src;
-  const imageUrl2 = Image2.src;
-  const imageUrl3 = Image3.src;
-
   // Array com as informações das imagens
   const imageInfos = [
     {
-      url: imageUrl1,
+      src: Image1,
       title: 'Harvesting Time',
       description: 'Elias Valtieri'
     },
     {
-      url: imageUrl2,
+      src: Image2,
       title: 'Blooming Field',
       description: 'Johannes Vermmer'
     },
     {
-      url: imageUrl3,
+      src: Image3,
       title: 'Retrato Moderno: série 01',
       description: 'B. Motta'
     }
   ];
 
-  // Estado para controlar o índice da imagem ativa
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // Função para avançar para a próxima imagem
   const nextSlide = () => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % imageInfos.length);
   };
 
-  // Função para retroceder para a imagem anterior
   const prevSlide = () => {
     setActiveIndex((prevIndex) => (prevIndex - 1 + imageInfos.length) % imageInfos.length);
   };
 
-  // Efeito para avançar automaticamente entre os slides
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveIndex((prevIndex) => (prevIndex + 1) % imageInfos.length);
-    }, 5000); // Intervalo de 5 segundos (5000 milissegundos)
+    }, 5000);
 
-    return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -63,7 +52,7 @@ const Spotlight = () => {
             className={`w-full h-full bg-cover bg-center absolute top-0 left-0 transition-opacity duration-500 ${
               index === activeIndex ? 'opacity-100' : 'opacity-0'
             }`}
-            style={{ backgroundImage: `url(${imageInfo.url})` }}
+            style={{ backgroundImage: `url(${imageInfo.src.src})` }} // Usar src.src para obter a URL da imagem
           >
             <div className="absolute bottom-4 left-4 bg-black bg-opacity-60 text-white p-4 rounded-sm ">
               <h2 className="text-lg italic">{imageInfo.title}</h2>
