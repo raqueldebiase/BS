79d500aa959dbafbe56d3100a961a84eaed22776
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-20 14:47:33 +0100

    add og:image change to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,13 +25,13 @@ export default function RootLayout({
         <Head>
         <meta property="og:title" content="Black Square" />
         <meta property="og:description" content="Digital Art Commerce" />
-        <meta property="og:image" content="https://blacksquare.vercel.app/opengraph.jpeg" />
+        <meta property="og:image" content="https://blacksquare.vercel.app/opengraph.jpg" />
         <meta property="og:url" content="https://blacksquare.vercel.app" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Black Square" />
         <meta name="twitter:description" content="Digital Art Commerce" />
-        <meta name="twitter:image" content="https://blacksquare.vercel.app/backgrounds/hero.jpeg" />
+        <meta name="twitter:image" content="https://blacksquare.vercel.app/opengraph.jpg" />
         </Head>
       <body className={inter.className}>
         <CartProvider>
